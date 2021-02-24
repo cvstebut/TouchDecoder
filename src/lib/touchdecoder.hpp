3a04352f74403dbda262a6ff33bdbe6950e8bef0
Christian von Stebut <christian@von.stebut.org>
2021-02-24 22:07:48 +0100

    wip: replaced hard coded timing with variables in lib and tests; enabled custom timing via constructor

--- src/lib/touchdecoder.hpp
+++ src/lib/touchdecoder.hpp
@@ -13,6 +13,18 @@ struct decoderOutput
 {
   std::bitset<decoderwidth> shortPress{};
   std::bitset<decoderwidth> longPress{};
+  std::bitset<decoderwidth> doubleShortPress{};
+  std::bitset<decoderwidth> doubleLongPress{};
+  std::bitset<decoderwidth> shortLongPress{};
+};
+
+struct touchDecoderTimingConfig
+{
+  unsigned long minReleaseTime{};
+  unsigned long shortPressTime{};
+  unsigned long longPressTime{};
+  unsigned long maxIdleShortTime{};
+  unsigned long maxIdleLongTime{};
 };
 
 class state_machine
@@ -20,9 +32,23 @@ class state_machine
   using Self = state_machine;
 
 public:
+  explicit state_machine(const touchDecoderTimingConfig &tc) : tc{tc} {}
+
   // States
   struct Idle
   {
+  };
+
+  struct IdleShort
+  {
+    unsigned long entry_time{};
+    int keyid{};
+  };
+
+  struct IdleLong
+  {
+    unsigned long entry_time{};
+    int keyid{};
   };
 
   struct Touched
@@ -30,17 +56,18 @@ public:
     unsigned long entry_time{};
   };
 
-  struct Short
-  {
-    unsigned long entry_time{};
-  };
-
-  struct Long
+  struct TouchedSecond
   {
     unsigned long entry_time{};
   };
 
   // Events
+  struct no_event
+  {
+    int keyid{};
+    unsigned long event_time{};
+  };
+
   struct touch_event
   {
     int keyid{};
@@ -56,30 +83,30 @@ public:
   {
     using namespace boost::sml;
 
-    auto guard_min_time = [](const auto &event, Touched &state) {
+    auto guard_min_touch_time = [this](const auto &event, Touched &state) {
       bool timeout = false;
       unsigned long timeDiff = event.event_time - state.entry_time;
-      if (timeDiff < 10)
+      if (timeDiff < tc.minReleaseTime)
       {
         timeout = true;
       }
-      std::cout << "Key " << event.keyid << " - guard_min_time (" << timeDiff << ") = " << timeout << std::endl;
+      std::cout << "Key " << event.keyid << " - guard_min_touch_time (" << timeDiff << ") = " << timeout << std::endl;
       return timeout;
     };
-    auto guard_short_press_time = [](const auto &event, Touched &state) {
+    auto guard_short_press_time = [this](const auto &event, Touched &state) {
       bool result = false;
       unsigned long timeDiff = event.event_time - state.entry_time;
-      if (timeDiff >= 10 && timeDiff < 20)
+      if (timeDiff >= tc.minReleaseTime && timeDiff < tc.shortPressTime)
       {
         result = true;
       }
       std::cout << "Key " << event.keyid << " - guard_short_press_time (" << timeDiff << ") = " << result << std::endl;
       return result;
     };
-    auto guard_long_press_time = [](const auto &event, Touched &state) {
+    auto guard_long_press_time = [this](const auto &event, Touched &state) {
       bool result = false;
       unsigned long timeDiff = event.event_time - state.entry_time;
-      if (timeDiff >= 30)
+      if (timeDiff >= tc.longPressTime)
       {
         result = true;
       }
@@ -87,68 +114,109 @@ public:
       return result;
     };
 
-    const auto touch_action = [](const auto &event, Touched & state) {
+    auto guard_max_idleshort_time = [this](const auto &event, IdleShort &state, unsigned long &currentTime) {
+      bool timeout = false;
+      unsigned long timeDiff = currentTime - state.entry_time;
+      if (timeDiff > tc.maxIdleShortTime)
+      {
+        timeout = true;
+      }
+      std::cout << "Key " << state.keyid << " - guard_max_idleshort_time (" << timeDiff << ") = " << timeout << std::endl;
+      return timeout;
+    };
+
+    auto guard_max_idlelong_time = [this](const auto &event, IdleLong &state, unsigned long &currentTime) {
+      bool timeout = false;
+      unsigned long timeDiff = currentTime - state.entry_time;
+      if (timeDiff > tc.maxIdleLongTime)
+      {
+        timeout = true;
+      }
+      std::cout << "Key " << state.keyid << " - guard_max_idlelong_time (" << timeDiff << ") = " << timeout << std::endl;
+      return timeout;
+    };
+
+    const auto touch_action = [](const auto &event, Touched &state) {
       std::cout << "Key " << event.keyid << " - Transition due to touch_event" << std::endl;
       state.entry_time = event.event_time;
     };
-    const auto release_short_action = [](decoderOutput &dcOutput, const auto &event) {
-      std::cout << "Key " << event.keyid << " - Transition due to release_short_action" << std::endl;
-      dcOutput.shortPress.set(event.keyid);
+
+    const auto release_to_idle_short = [](decoderOutput &dcOutput, IdleShort &state, const auto &event) {
+      std::cout << "Key " << event.keyid << " - Transition due to release_to_idle_short" << std::endl;
+      state.entry_time = event.event_time;
+      state.keyid = event.keyid;
     };
-    const auto release_long_action = [](decoderOutput &dcOutput, const auto &event) {
-      std::cout << "Key " << event.keyid << " - Transition due to release_long_action" << std::endl;
-      dcOutput.longPress.set(event.keyid);
+
+    const auto release_to_idle_long = [](decoderOutput &dcOutput, IdleLong &state, const auto &event) {
+      std::cout << "Key " << event.keyid << " - Transition due to release_to_idle_long" << std::endl;
+      state.entry_time = event.event_time;
+      state.keyid = event.keyid;
     };
+
+    const auto release_short_action = [](decoderOutput &dcOutput, IdleShort &state, const auto &event) {
+      std::cout << "Key " << state.keyid << " - Transition due to release_short_action" << std::endl;
+      dcOutput.shortPress.set(state.keyid);
+    };
+
+    const auto release_long_action = [](decoderOutput &dcOutput, IdleLong &state, const auto &event) {
+      std::cout << "Key " << state.keyid << " - Transition due to release_long_action" << std::endl;
+      dcOutput.longPress.set(state.keyid);
+    };
+
     const auto timeout_action = [](decoderOutput &dcOutput, const auto &event) {
       std::cout << "Key " << event.keyid << " - Transition due to timeout_action" << std::endl;
     };
+
     // clang-format off
     return make_transition_table(
-        * state<Idle>      + event<touch_event>                                / (touch_action)         = state<Touched>
-        , state<Touched>   + event<release_event>   [guard_min_time]           / (timeout_action)       = state<Idle>
-        , state<Touched>   + event<release_event>   [guard_short_press_time]   / (release_short_action) = state<Idle>
-        , state<Touched>   + event<release_event>   [guard_long_press_time]    / (release_long_action)  = state<Idle>
+        * state<Idle>              + event<touch_event>                                  / (touch_action)          = state<Touched>
+        , state<Touched>           + event<release_event>   [guard_min_touch_time]       / (timeout_action)        = state<Idle>
+        , state<Touched>           + event<release_event>   [guard_short_press_time]     / (release_to_idle_short) = state<IdleShort>
+        , state<Touched>           + event<release_event>   [guard_long_press_time]      / (release_to_idle_long)  = state<IdleLong>
+        , state<IdleShort>                                  [guard_max_idleshort_time]   / (release_short_action)  = state<Idle>
+        , state<IdleLong>                                   [guard_max_idlelong_time]    / (release_long_action)   = state<Idle>
     );
     // clang-format on
   }
-  bool guard3(int i) const noexcept
-  {
-    assert(42 == i);
-    std::cout << "guard3" << std::endl;
-    return true;
-  }
+
+  touchDecoderTimingConfig tc{};
 };
 
 class TouchDecoder
 {
 public:
-  state_machine statemachine{};
+  explicit TouchDecoder(): statemachine(state_machine{touchDecoderTimingConfig{10, 20, 30, 50, 50}}){}
+  explicit TouchDecoder(touchDecoderTimingConfig);
+
+  state_machine statemachine;
+
   boost::sml::sm<state_machine> smarray[decoderwidth] = {
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output},
-      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output}};
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime},
+      boost::sml::sm<state_machine>{statemachine, state_machine::Touched{0}, output, currentTime}};
 
   void push(uint16_t touchstate, unsigned long pushTime)
   {
     std::bitset<decoderwidth> newState = std::bitset<decoderwidth>(touchstate);
     _released = current & ~newState;
     _touched = ~current & newState;
-    output.shortPress = 0;
-    output.longPress = 0;
-    currenttime = pushTime;
+
+    // reset output event registers to zero
+    output = decoderOutput{};
 
     std::cout << "PRE-SM  - current: " << current << ", touchstate: " << touchstate << ", _released: " << _released << ", _touched: " << _touched << ", shortPress: " << output.shortPress << ", longPress: " << output.longPress << std::endl;
 
     current = newState;
+    currentTime = pushTime;
 
     for (int i = 0; i < decoderwidth; i++)
     {
@@ -163,6 +231,15 @@ public:
       if (_released.test(i))
       {
         smarray[i].process_event(state_machine::release_event{i, pushTime});
+      }
+    }
+
+    for (int i = 0; i < decoderwidth; i++)
+    {
+      if (~_released.test(i) && ~_touched.test(i))
+      {
+        std::cout << "Key " << i << " - processing no_event to enable timeout handling without released or touched" << std::endl;
+        smarray[i].process_event(state_machine::no_event{i, pushTime});
       }
     }
     std::cout << "POST-SM - current: " << current << ", touchstate: " << touchstate << ", _released: " << _released << ", _touched: " << _touched << ", shortPress: " << output.shortPress << ", longPress: " << output.longPress << std::endl;
@@ -183,16 +260,33 @@ public:
     return output.longPress;
   }
 
+  std::bitset<decoderwidth> doubleShortPress()
+  {
+    return output.doubleShortPress;
+  }
+
+  std::bitset<decoderwidth> doubleLongPress()
+  {
+    return output.doubleLongPress;
+  }
+
+  std::bitset<decoderwidth> shortLongPress()
+  {
+    return output.shortLongPress;
+  }
+
   int width()
   {
     return decoderwidth;
   }
 
 private:
-  unsigned long currenttime;
+  unsigned long currentTime;
   int empMaxTouched; // max count of touches in buffer. If exceeded will clear buffer and set empDetected = true
   int _empTouchCount = 0;
   int bufSize;
+
+  touchDecoderTimingConfig tc{10, 20};
 
   decoderOutput output;
   std::bitset<decoderwidth> current = 0;
@@ -201,3 +295,5 @@ private:
 
   // actions
 };
+
+TouchDecoder::TouchDecoder(touchDecoderTimingConfig tc) : statemachine{tc} {}
--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -13,26 +13,114 @@ TEST_CASE("Construction ok")
   }
 }
 
-TEST_CASE("try some pushing")
+TEST_CASE("Construction with custom timing")
 {
-  TouchDecoder decoder;
-  std::bitset<decoderwidth> bs;
+  touchDecoderTimingConfig tc{
+      tc.minReleaseTime = 10,
+      tc.shortPressTime = 20,
+      tc.longPressTime = 30,
+      tc.maxIdleShortTime = 50,
+      tc.maxIdleLongTime = 50};
+  TouchDecoder decoder{tc};
+  SECTION("timing in decoder's state machine should match custom timing passed to decoder constructor")
+  {
+    REQUIRE(decoder.statemachine.tc.minReleaseTime == tc.minReleaseTime);
+    REQUIRE(decoder.statemachine.tc.shortPressTime == tc.shortPressTime);
+    REQUIRE(decoder.statemachine.tc.longPressTime == tc.longPressTime);
+    REQUIRE(decoder.statemachine.tc.maxIdleShortTime == tc.maxIdleShortTime);
+    REQUIRE(decoder.statemachine.tc.maxIdleLongTime == tc.maxIdleLongTime);
+  }
+}
+
+TEST_CASE("decoder - release before min time")
+{
+  touchDecoderTimingConfig tc{
+      tc.minReleaseTime = 10,
+      tc.shortPressTime = 20,
+      tc.longPressTime = 30,
+      tc.maxIdleShortTime = 50,
+      tc.maxIdleLongTime = 50};
+  TouchDecoder decoder{tc};
+
+  unsigned long current{};
 
   decoder.push(0, 0);
-  decoder.push(1, 10);
-  decoder.push(0, 15);
-  decoder.push(3, 20);
-  decoder.push(3, 30);
-  decoder.push(15, 40);
-  decoder.push(15, 50);
-  decoder.push(7, 60);
-  decoder.push(3, 70);
-  decoder.push(1, 80);
-  decoder.push(3, 90);
-  decoder.push(0, 100);
-  decoder.push(0, 110);
-  SECTION("should print state changes")
+  decoder.push(1, current += tc.minReleaseTime);
+  decoder.push(0, current += (tc.minReleaseTime / 2));
+  SECTION("should disregard both previous touch and current release event and return to idle state")
   {
-    REQUIRE(1 == 1);
+    REQUIRE(decoder.shortPress() == 0);
+    REQUIRE(decoder.statemachine.tc.minReleaseTime == tc.minReleaseTime);
+  }
+}
+
+TEST_CASE("decoder - custom timing - release before min time")
+{
+  touchDecoderTimingConfig tc{
+      tc.minReleaseTime = 20,
+      tc.shortPressTime = 25,
+      tc.longPressTime = 30,
+      tc.maxIdleShortTime = 50,
+      tc.maxIdleLongTime = 50};
+  TouchDecoder decoder{tc};
+
+  unsigned long current{};
+
+  decoder.push(0, 0);
+  decoder.push(1, current += tc.minReleaseTime);
+  decoder.push(0, current += (tc.minReleaseTime / 2));
+  SECTION("should disregard both previous touch and current release event and return to idle state")
+  {
+    REQUIRE(decoder.shortPress() == 0);
+    REQUIRE(decoder.statemachine.tc.minReleaseTime == tc.minReleaseTime);
+  }
+}
+
+
+TEST_CASE("decoder - release within short time - next push after all timeouts expired")
+{
+  touchDecoderTimingConfig tc{
+      tc.minReleaseTime = 10,
+      tc.shortPressTime = 20,
+      tc.longPressTime = 30,
+      tc.maxIdleShortTime = 50,
+      tc.maxIdleLongTime = 50};
+  TouchDecoder decoder{tc};
+
+  unsigned long current{};
+
+  decoder.push(0, 0);
+  decoder.push(1, current += tc.minReleaseTime + 1);
+  decoder.push(0, current += tc.shortPressTime - 1);
+  decoder.push(0, current += tc.maxIdleShortTime + 1);
+
+  SECTION("should fire short press")
+  {
+    REQUIRE(decoder.shortPress() == 1);
+    REQUIRE(decoder.longPress() == 0);
+  }
+}
+
+TEST_CASE("decoder - release within long time - next push after all timeouts expired")
+{
+  touchDecoderTimingConfig tc{
+      tc.minReleaseTime = 10,
+      tc.shortPressTime = 20,
+      tc.longPressTime = 30,
+      tc.maxIdleShortTime = 50,
+      tc.maxIdleLongTime = 50};
+  TouchDecoder decoder{tc};
+
+  unsigned long current{};
+
+  decoder.push(0, 0);
+  decoder.push(1, current += tc.minReleaseTime + 1);
+  decoder.push(0, current += tc.longPressTime + 1);
+  decoder.push(0, current += tc.maxIdleLongTime + 1);
+
+  SECTION("should fire long press")
+  {
+    REQUIRE(decoder.shortPress() == 0);
+    REQUIRE(decoder.longPress() == 1);
   }
 }
